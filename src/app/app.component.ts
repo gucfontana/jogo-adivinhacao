739f9e676a6fd3ac083b48ac4d7dd6baa53cb20a
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-09-11 15:30:40 -0300

    criando arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jogo-adivinhacao';
+  title: string = 'Jogo de Adivinhação';
+  guess: number = 0;
+  message: string = '';
+  private targetNumber: number = 42;
+
+  checkGuess() {
+    if (this.guess === this.targetNumber) {
+      this.message = 'Parabéns! Você acertou!';
+    } else if (this.guess < this.targetNumber) {
+      this.message = 'Tente um número maior.';
+    } else {
+      this.message = 'Tente um número menor.';
+    }
+  }
 }
